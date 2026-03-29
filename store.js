3a2fd01071217be3e00ab0dd46177fda07ed8f3f
DLIFE21 <darlin45franco.brito@gmail.com>
2026-03-29 12:20:59 -0400

    Update store.js

--- store.js
+++ store.js
@@ -5,19 +5,19 @@ const STORE = {
             id: 1,
             name: "Cargador USB-C 20W Apple",
             price: 1200,
-            image: "https://via.placeholder.com/200"
+            image: "https://i.imgur.com/awRBPUu.jpeg"
         },
         {
             id: 2,
             name: "Cable Lightning Original",
-            price: 900,
-            image: "https://via.placeholder.com/200"
+            price: 700,
+            image: "https://i.imgur.com/XasaDsN.jpeg"
         },
         {
             id: 3,
             name: "AirPods Pro 2",
             price: 13500,
-            image: "https://via.placeholder.com/200"
+            image: "https://i.imgur.com/VDyEZgw.jpeg"
         },
         {
             id: 4,
@@ -27,6 +27,24 @@ const STORE = {
         },
         {
             id: 5,
+            name: "Power Bank 10,000mAh",
+            price: 1800,
+            image: "https://i.imgur.com/mCRwG2c.png"
+        },
+        {
+            id: 7,
+            name: "Power Bank 10,000mAh",
+            price: 1800,
+            image: "https://via.placeholder.com/200"
+        },
+        {
+            id: 8,
+            name: "Power Bank 10,000mAh",
+            price: 1800,
+            image: "https://via.placeholder.com/200"
+        },
+        {
+            id: 9,
             name: "Power Bank 10,000mAh",
             price: 1800,
             image: "https://via.placeholder.com/200"
@@ -40,7 +58,7 @@ const STORE = {
             name: "iPhone 16",
             tipo: "iPhone",
             price: 36600,
-            stock: 29,
+            stock: 28,
             image: "https://i.imgur.com/I02Qzbr.jpeg"
         },
         {
@@ -53,8 +71,16 @@ const STORE = {
         },
 
         // ⚡ GAMA MEDIA
-        {
+       {
             id: 103,
+            name: "iPhone 15 Plus",
+            tipo: "iPhone",
+            price: 26100,
+            stock: 1,
+            image: "https://i.imgur.com/FI6Z7pO.png"
+        },
+        {
+            id: 104,
             name: "iPhone 15",
             tipo: "iPhone",
             price: 22950,
@@ -62,22 +88,46 @@ const STORE = {
             image: "https://i.imgur.com/2rbwdyN.png"
         },
         {
-            id: 104,
-            name: "iPhone 15 Plus",
+            id: 105,
+            name: "iPhone 14 Plus",
             tipo: "iPhone",
             price: 26100,
-            stock: 1,
+            stock: 0,
+            image: "https://i.imgur.com/FI6Z7pO.png"
+        },
+        {
+            id: 106,
+            name: "iPhone 14",
+            tipo: "iPhone",
+            price: 26100,
+            stock: 0,
             image: "https://i.imgur.com/FI6Z7pO.png"
         },
 
         // 💸 ECONÓMICOS
         {
-            id: 105,
+            id: 107,
             name: "iPhone 13",
             tipo: "iPhone",
             price: 16500,
-            stock: 3,
+            stock: 4,
             image: "https://i.imgur.com/FBxVP3I.png"
+        },
+        {
+            id: 108,
+            name: "iPhone 12",
+            tipo: "iPhone",
+            price: 16500,
+            stock: 0,
+            image: "https://i.imgur.com/fE29r73.jpeg"
+        },
+        {
+            id: 109,
+            name: "iPhone 11",
+            tipo: "iPhone",
+            price: 16500,
+            stock: 5,
+            image: "https://i.imgur.com/fE29r73.jpeg"
         },
         // 💻 MacBook
     {
@@ -85,7 +135,7 @@ const STORE = {
         tipo: "MacBook",
         name: "MacBook Air M2",
         price: 65000,
-        stock: 4,
+        stock: 0,
         image: "https://via.placeholder.com/200"
     },
     {
@@ -103,7 +153,7 @@ const STORE = {
         tipo: "iPad",
         name: "iPad 10",
         price: 28000,
-        stock: 6,
+        stock: 0,
         image: "https://via.placeholder.com/200"
     }
 ],
